5ef316c27dda075cd9dfc1eb42e40e57be9f5d33
Án Bình Trọng <minhtam.seta@gmail.com>
2014-01-10 13:38:27 +0700

    Add Video code

---
+++ PlayVideo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PlayVideo.Startup))]
+namespace PlayVideo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
